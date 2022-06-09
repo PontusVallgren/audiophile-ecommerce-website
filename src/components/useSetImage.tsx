82ab54b30f73a98a0acaf15229a1414b4e0958cb
PontusVallgren <pontus.wallgren92@gmail.com>
2022-06-09 14:40:05 +0200

    Create hook for changing images depending on device width

---
+++ src/components/useSetImage.tsx
@@ -0,0 +1,40 @@
+import { StaticImageData } from "next/image";
+import { useEffect, useState } from "react";
+
+const useSetImage = (
+  mobile: StaticImageData,
+  tablet: StaticImageData,
+  desktop: StaticImageData
+) => {
+  const [promoImage, setPromoImage] = useState(mobile);
+
+  useEffect(() => {
+    if (window.innerWidth < 768) {
+      setPromoImage(mobile);
+    }
+    if (window.innerWidth >= 768) {
+      setPromoImage(tablet);
+    }
+    if (window.innerWidth >= 1024) {
+      setPromoImage(desktop);
+    }
+
+    const updateMedia = () => {
+      if (window.innerWidth < 768) {
+        setPromoImage(mobile);
+      }
+      if (window.innerWidth >= 768) {
+        setPromoImage(tablet);
+      }
+      if (window.innerWidth >= 1024) {
+        setPromoImage(desktop);
+      }
+    };
+    window.addEventListener("resize", updateMedia);
+    return () => window.removeEventListener("resize", updateMedia);
+  }, [desktop, mobile, tablet]);
+
+  return promoImage;
+};
+
+export default useSetImage;
