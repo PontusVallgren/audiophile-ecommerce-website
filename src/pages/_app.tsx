53e0c12f2f26fef4240df22f26c91fdd4e241bc8
PontusVallgren <pontus.wallgren92@gmail.com>
2022-06-12 02:35:52 +0200

    implement footer and header on every page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,20 @@
 import "../../styles/globals.css";
 import type { AppProps } from "next/app";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <header>
+        <Navbar />
+      </header>
+      <main>
+        <Component {...pageProps} />
+      </main>
+      <Footer />
+    </>
+  );
 }
 
 export default MyApp;
