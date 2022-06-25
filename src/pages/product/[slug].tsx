a9e0848f8dc31445087b0682437a3e631df5b464
PontusVallgren <pontus.wallgren92@gmail.com>
2022-06-25 21:31:03 +0200

    create path for specific product

--- src/pages/product/[slug].tsx
+++ src/pages/product/[slug].tsx
@@ -1,19 +1,33 @@
 import { GetServerSideProps, NextPage } from "next";
 import React from "react";
+import db from "../../utils/db";
+import Products from "../../models/Product";
+import { product } from "../../types";
+import ProductSpec from "../../components/ProductSpec";
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
-  const id = context.params!.slug;
-  console.log(id);
+  await db.connect();
+  const slug = context.params!.slug;
+  const res = await Products.findOne({ slug });
+  const products = JSON.parse(JSON.stringify(res));
 
   return {
     props: {
-      id: id,
+      products: db.convertDocToObj(products),
     },
   };
 };
 
-const Product: NextPage = () => {
-  return <div>Checkout my product</div>;
+type ProductProps = {
+  products: product;
+};
+
+const Product: NextPage<ProductProps> = ({ products }) => {
+  return (
+    <div>
+      <ProductSpec data={products} />
+    </div>
+  );
 };
 
 export default Product;
