11fb1dbde64263e42d4cab9c0631cc5756c1c8b7
PontusVallgren <pontus.wallgren92@gmail.com>
2022-06-25 20:45:15 +0200

    Create shopping cart with context and useReducer to create actions to add items into cart and store them in cookies

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,17 +2,20 @@ import "../../styles/globals.css";
 import type { AppProps } from "next/app";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
+import { StoreProvider } from "../utils/Store";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
-      <header>
-        <Navbar />
-      </header>
-      <main>
-        <Component {...pageProps} />
-      </main>
-      <Footer />
+      <StoreProvider>
+        <header>
+          <Navbar />
+        </header>
+        <main>
+          <Component {...pageProps} />
+        </main>
+        <Footer />
+      </StoreProvider>
     </>
   );
 }
