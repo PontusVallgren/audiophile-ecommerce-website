cf9633eef22cbdf87a16b816fe3f4350b23fa041
PontusVallgren <pontus.wallgren92@gmail.com>
2022-06-25 20:48:32 +0200

    add images for products and create converter for db _id

--- src/utils/db.ts
+++ src/utils/db.ts
@@ -1,43 +1,5 @@
 import mongoose from "mongoose";
-
-/* const connection = {};
-
-const connect = async () => {
-  if (connection.isConnected) {
-    console.log("Already connected");
-    return;
-  }
-  if (mongoose.connections.length > 0) {
-    connection.isConnected = mongoose.connections[0].readyState;
-    if (connection.isConnected === 1) {
-      console.log("use previous connection");
-      return;
-    }
-    await mongoose.disconnect();
-  }
-  const db = await mongoose.connect(process.env.MONGODB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-  });
-  console.log("new connection");
-  connection.isConnected = db.connections[0].readyState;
-};
-
-const disconnect = async () => {
-  if(connection.isConnected) {
-    if(process.env.NODE_ENV === 'production') {
-      await mongoose.disconnect()
-      connection.isConnected = false;
-    } else {
-      console.log('not disconnected')
-    }
-  }
-}
-
-const db = {connect, disconnect };
-
-export default db */
+import { product } from "../types";
  
 const connect = async () => {
   await mongoose.connect(process.env.MONGODB_URI!)
@@ -49,6 +11,11 @@ const disconnect = async () => {
   console.log('disconnect DB')
 }
 
-const db = {connect, disconnect}
+const convertDocToObj = (doc: product) => {
+  doc._id = doc._id.toString();
+  return doc;
+}
+
+const db = {connect, disconnect, convertDocToObj}
 
 export default db;
