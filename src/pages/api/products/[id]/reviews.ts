31dd8207be10a1f5fff97f3dcb84b53cdabb0a31
PontusVallgren <pontus.wallgren92@gmail.com>
2022-06-29 21:04:11 +0200

    Create pagination on reviews api

--- src/pages/api/products/[id]/reviews.ts
+++ src/pages/api/products/[id]/reviews.ts
@@ -1,6 +1,9 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import Product from '../../../../models/Product'
 import db from '../../../../utils/db'
+import { paginate } from '../../../../utils/filters';
+
+
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     const id = req.query.id;
@@ -8,7 +11,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
         await db.connect();
         const data = await Product.findById(id, {reviews: 1});
         await db.disconnect();
-        res.status(200).send(data);
+        res.status(200).send(paginate(data, req.query.page as string));
     } else if (req.method === "POST") {
         await db.connect();
         await Product.findByIdAndUpdate(id, {
