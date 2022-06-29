57cdf65b055a42b71ad11a9fa080fcfbed2bf4e1
PontusVallgren <pontus.wallgren92@gmail.com>
2022-06-29 11:48:17 +0200

    create api for updating database with reviews when http request using POST

--- src/pages/api/products/[id]/reviews.ts
+++ src/pages/api/products/[id]/reviews.ts
@@ -3,11 +3,26 @@ import Product from '../../../../models/Product'
 import db from '../../../../utils/db'
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+    const id = req.query.id;
     if (req.method === "GET") {
         await db.connect();
-        const data = await Product.findById(req.query.id, {reviews: 1});
+        const data = await Product.findById(id, {reviews: 1});
         await db.disconnect();
-        res.send(data);
+        res.status(200).send(data);
+    } else if (req.method === "POST") {
+        await db.connect();
+        await Product.findByIdAndUpdate(id, {
+            $push: {
+                reviews: {
+                    name: req.body.name,
+                    rating: req.body.rating,
+                    comment: req.body.comment,
+                }
+            }
+        });
+        db.disconnect();
+        res.status(201).send(req.body);
     }
+
 
 };
